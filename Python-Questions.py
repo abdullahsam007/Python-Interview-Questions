955bde783ec10fcfa11fe83d7c22bd87dab84af7
Abdullah <abdullah.habib@arbisoft.com>
2023-10-26 13:52:20 +0500

    Python Questions

---
+++ Python-Questions.py
@@ -0,0 +1,15 @@
+# # Find Common letter between two strings
+
+# def common():
+#     a = input("Enter string 1: ")
+#     b = input("Enter string 2: ")
+
+#     c = set(a)
+#     d = set(b)
+
+#     commonLogic = c & d
+
+#     print(commonLogic)
+
+# common()
+
