3f4a52d359e05a1c95851e45adff7e2d7a1e8b51
Abdullah <abdullah.habib@arbisoft.com>
2023-10-26 16:39:04 +0500

    more Q's

--- Python-Questions.py
+++ Python-Questions.py
@@ -1,4 +1,22 @@
-# # Find Common letter between two strings
+# Return true if there's duplicate in string else false
+
+# arr = [1,2,3,4,5,1,2]
+
+# def duplicate(arr):
+
+#     return len(arr) != len(set(arr))
+# duplicate()
+
+
+
+# See if the given two strings anagram
+# def anagram(s, t):
+#     return sorted(s) == sorted(t)
+
+# anagram()
+
+
+# Find Common letter between two strings
 
 # def common():
 #     a = input("Enter string 1: ")
@@ -15,4 +33,36 @@
 
 
 # Count the frequency of the words
+
+# def frequency():
+#     a = input("Enter a string: ")
+#     seperator = a.split()
+#     d = {}
+
+#     for i in seperator:
+#         if i not in d.keys():
+#             d[i] = 0
+#         d[i] = d[i] + 1
+
+#     print(d)
+# frequency()
+
+
+
+# Convert two lists into a dictionary
+
+# def list_to_dic():
+#     list1 = ["One", "Two", "Three"]
+#     list2 = [1, 2, 3]
+
+#     my_dict = dict(zip(list1, list2))
+#     print(my_dict)
+
+# list_to_dic()
+
+
+#Find Out Pairs with given sum in an array in python of time complexity O(n log n)
+arr = [5, 7, 4, 3, 9, 8, 19, 21]
+sum = 17;
+
 
--- Topic-Wise-Questions.py
+++ Topic-Wise-Questions.py
@@ -2,7 +2,44 @@
 # If-else Statements
 # Loops
 # Math
-# Arrays
+# ----------Arrays----------
+
+# 1) Reverse an array of intergers
+
+# arr = [1,2,3,4,5]
+# arr.sort(reverse=True)
+
+# print(arr)
+
+# 2) Mix and Max in array
+
+# arr = [1,23,4,5,6,78]
+# ma = max(arr)
+# mi = min(arr)
+
+# print("Max: ", ma)
+# print("Min: ", mi)
+
+# 3) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Strings
 # Queues
 # HashSets / Sets
